cdae3a2946ee115f31c9e6b641f7b19e124a126f
Matthew Crawford <matthew.crawford92@gmail.com>
2020-11-12 22:48:30 -0800

    Implemented working API calls to database

--- server/index.js
+++ server/index.js
@@ -2,6 +2,40 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const { Question } = require('../database/index.js');
+const { User } = require('../database/index.js');
+const { Answer } = require('../database/index.js');
+
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/users', function(req, res) {
+  var allUsers = User.find().exec((err, data) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send(data)
+    }
+  })
+})
+
+app.get('/questions', function(req, res) {
+  var allUsers = Question.find().exec((err, data) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send(data)
+    }
+  })
+})
+
+app.get('/answers', function(req, res) {
+  var allUsers = Answer.find().exec((err, data) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send(data)
+    }
+  })
+})
 
 app.listen(port, () => console.log(`listening on port ${port}`))

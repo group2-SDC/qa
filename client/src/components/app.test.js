673623ccbabece54620a867af961da4cc963a509
Matthew Crawford <matthew.crawford92@gmail.com>
2020-11-14 14:51:18 -0800

    Added a few changes to app.test.js

--- client/src/components/app.test.js
+++ client/src/components/app.test.js
@@ -3,8 +3,12 @@ import { shallow, mount, render } from 'enzyme';
 import App from './App.jsx';
 
 describe('<App />', () => {
+  let wrapper;
+  beforeEach(() => { wrapper = shallow(<App />)})
   it('renders a div', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find('div').text()).toContain('Hello World!');
   });
+  it('contains one div with class test', () => {
+    expect(wrapper.find('div.test')).to.have.lengthof(1);
+  })
 });

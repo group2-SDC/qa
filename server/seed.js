813069191f38adfbfc88bd00e36cdc511196ba59
Matthew Crawford <matthew.crawford92@gmail.com>
2020-11-13 21:07:59 -0800

    Finally got refactored seed file completed!

--- server/seed.js
+++ server/seed.js
@@ -1,23 +1,24 @@
 var faker = require('faker');
 const mongoose = require('mongoose');
 const { QuestionSet } = require('../database/index.js');
-// const { save } = require('../database/index.js');
+const { save } = require('../database/index.js');
 
 //To return all from these collections on command line
   //db.users.find() or db.questions.find() or db.answers.find()
 //When you're working on a branch and ready to push, do...
 //"git push origin" and then the branch name
 //Also think of it like git push <place i am pushing to> <thing i am pushing>
-//git pull combines branches
+// for a git pull
+  // switch back to main branch on github
+  // run git pull origin main from vs code (recommended by Joel- this will pull the main from github down to my local repo)
+  // OR (also recommended by Joel) just switch to branch main and run git merge <branch name>
 
 var createNewData = (i) => {
-  var randomNumQuestions = Math.floor(Math.random() * 20)
-  var randomNumAnswers = Math.floor(Math.random() * 4)
-  console.log('i inside of createNewData: ', i)
-  var newQuestionSet = new QuestionSet ({
-    primaryRecord: i,
-    questions: [
-      {
+  var randomNumQuestions = Math.floor(Math.random() * 10) + 1;
+  var questionsArray = [];
+  var answersArray = []
+  for (var i = 0; i < randomNumQuestions; i++) {
+    var question = {
         username: faker.name.findName(),
         profilePic: faker.image.imageUrl(),
         date: faker.date.past(),
@@ -25,24 +26,44 @@ var createNewData = (i) => {
         numContributions: Math.floor(Math.random() * 200),
         numHelpfulVotes: Math.floor(Math.random() * 50),
         question: faker.lorem.sentence(),
-        answers: [
-          {
-            username: faker.name.findName(),
-            profilePic: faker.image.imageUrl(),
-            date: faker.date.past(),
-            answer: faker.lorem.sentence()
-          }
-        ]
-      }
-    ]
-  })
-  newQuestionSet.save((err) => {
-    if (err) {
-      console.log("Err: ", err)
+        answer: []
     }
+    // for (var j = 0; j < randomNumAnswers; j++) {
+    //   var answer = {
+    //     ansUsername: faker.name.findName(),
+    //     ansProfilePic: faker.image.imageUrl(),
+    //     ansDate: faker.date.past(),
+    //     ansAnswer: faker.lorem.sentence()
+    //   };
+    //   answersArray.push(answer)
+    //   question.answer.push(answer)
+    // }
+    questionsArray.push(question)
+  }
+  // console.log('questionsArray[0].answer: ', questionsArray[0].answer)
+  var newQuestionSet = new QuestionSet ({
+    primaryRecord: i,
+    questions: questionsArray
   })
+
+  console.log('newQuestionSet.questions=======>', newQuestionSet.questions)
+  for (let j = 0; j < newQuestionSet.questions.length; j++) {
+    var randomNumAnswers = Math.floor(Math.random() * 3) + 1;
+    for (let k = 0; k < randomNumAnswers; k++) {
+      var answer = {
+        ansUsername: faker.name.findName(),
+        ansProfilePic: faker.image.imageUrl(),
+        ansDate: faker.date.past(),
+        ansAnswer: faker.lorem.sentence()
+      };
+      newQuestionSet.questions[j].answers.push(answer);
+    }
+  }
+  console.log('new question set=====>', newQuestionSet.questions)
+
+  save(newQuestionSet);
+
 }
-//it seems like the saving of new question sets is running asynchronously, is this a problem? AKA primary records are getting generated out of order
 
 var generateNRecords = (n) => {
   for (var i = 0; i < n; i++) {
@@ -51,66 +72,5 @@ var generateNRecords = (n) => {
   }
 }
 
-generateNRecords(100);
-
-// var createNewData = () => {
-//   var newQuestion = new Question ({
-//     date: faker.date.past(),
-//     question: faker.lorem.sentence()
-//   });
-//   newQuestion.save((err) => {
-//     if (err) {
-//       console.log('THIS WAS AN ERROR: ', err)
-//     }
-//   })
-//   var newUser = new User ({
-//     username: faker.name.findName(),
-//     profilePic: faker.image.imageUrl(),
-//     location: faker.address.city() + ', ' + faker.address.country(),
-//     numContributions: Math.floor(Math.random() * 200),
-//     numHelpfulVotes: Math.floor(Math.random() * 50)
-//   });
-//   newUser.save((err) => {
-//     if (err) {
-//       console.log('THIS WAS AN ERROR: ', err)
-//     }
-//   })
-//   var newAnswer = new Answer ({
-//     date: faker.date.past(),
-//     answer: faker.lorem.sentence(),
-//     likes: Math.floor(Math.random() * 20)
-//   })
-//   newAnswer.save((err) => {
-//     if (err) {
-//       console.log('THIS WAS AN ERROR: ', err)
-//     }
-//   })
-// }
-
-
-// [
-//   primary record/location: 0-99 (for loop),
-//   questions (random number between 0 and 20?): [
-//     {
-//       username: random
-//       date: random
-//       location: random
-//       profilePic: random
-//       question: text (random)
-//       answers (random number betw 0 and 4?): [
-//         {
-//           username: random
-//           profilePic: random
-//           date: random
-//           answer: text (random)
-//         }
-//         {
-//           ...
-//         }
-//       ]
-//     }
-//     {
-//       ...(this is another question, perhaps with answers)
-//     }
-//   ]
-// ]
+generateNRecords(3);
+//will need to update to 100

f9a11f2deaea3ef376e7adbee09d66b745d967eb
Matthew Crawford <matthew.crawford92@gmail.com>
2020-11-15 10:02:03 -0800

    Started creating react skeleton

---
+++ client/src/components/Questions.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Questions extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>Testing questions.jsx</div>
+    )
+  }
+}
+
+export default Questions;
--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+import Questions from './questions.jsx';
+
 
 const Button = styled.button`
   font-family: sans-serif;
@@ -11,22 +14,43 @@ const Button = styled.button`
   &:hover {
     background-color: #424040
   }
-
 `
 
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.fetchQuestions = this.fetchQuestions.bind(this)
+    this.state = {
+      questions: []
+    }
   }
+
+  fetchQuestions() {
+    axios.get('/questions')
+    .then((response) => {
+      this.setState({
+        questions: response.data
+      })
+    }).catch((error) => {
+      console.log('THERE WAS AN ERROR: ', error)
+    })
+  }
+
+  componentDidMount() {
+    this.fetchQuestions()
+  }
+
   render() {
     return (
       <div className="parent">
         <div className="test">Hello World!</div>
         <Button>Next</Button>
+        <Questions questions={this.state.questions} />
       </div>
     )
   }
+
 }
 
 export default App;

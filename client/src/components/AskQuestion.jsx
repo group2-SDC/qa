2657d5449d7cf909394e4c2c7903cb7d51c1c271
Matthew Crawford <matthew.crawford92@gmail.com>
2020-11-19 16:32:16 -0800

    working on modal

--- client/src/components/AskQuestion.jsx
+++ client/src/components/AskQuestion.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import Modal from './Modal.jsx';
 
 const Header = styled.div `
   background-color: rgb(255, 255, 255);
-  margin-bottom: 24px;
-  padding: 0 16px;
 `
 const AskQuestionBar = styled.form `
 
@@ -14,17 +13,69 @@ const TextField = styled.textarea `
 TextField.displayName = 'TextField';
 
 const SubmitQButton = styled.button`
+  background-color: rgb(0, 0, 0);
+  border-color: rgb(0, 0, 0);
+  border-radius: 3px;
+  border-width: 1px;
+  display: block;
+  color: rgb(255, 255, 255);
+  // Will need to fix float
+  padding-bottom: 8px;
+  padding-top: 8px;
+  padding-right: 16px;
+  padding-left: 16px;
+  text-align: center;
+  height: 36px;
+  width: 126px;
 `
 SubmitQButton.displayName = 'SubmitQButton'
+
+const LowerSpanDiv = styled.div `
+padding: 0 24px;
+margin-bottom: 24px;
+`
+const Styling1 = styled.div `
+  padding-top: 16px;
+
+`
+const Styling2 = styled.div `
+padding-bottom: 18px;
+`
+const TopSpan = styled.div `
+  padding: 16px 12px;
+  font-size: 14px;
+  line-height: 18px;
+  text-align: center;
+  position: relative;
+  font-weight: 500;
+`
+const TopSpanDiv = styled.div `
+  border: solid #e0e0e0;
+  border-width: 1px;
+`
+const QandA = styled.div `
+  display: block;
+  width: 60%;
+  font-size: 28px;
+  font-weight: 500;
+`
+const SeeAll = styled.div `
+  font-size: 14px;
+  font-weight: 500;
+`
 
 class AskQuestion extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      question: ''
+      question: '',
+      showModal: false
+      // WILL NEED TO SWITCH BACK TO DEFAULT VALUE FALSE
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
   }
 
   handleChange(event) {
@@ -43,18 +94,69 @@ class AskQuestion extends React.Component {
       question: ''
     })
   }
+  showModal() {
+    this.setState({
+      showModal: true
+    })
+  }
+  hideModal() {
+    this.setState({
+      showModal: false
+    })
+  }
   render() {
-    return (
-      <Header>
-        <i className="far fa-comment-alt"></i>
-        <div>{this.props.questions.length} Q & A</div>
-        <div>Questions & Answers</div>
-        <AskQuestionBar>
-          <TextField onChange={this.handleChange} value={this.state.question} name="question"></TextField>
-          <SubmitQButton onClick={this.handleSubmit}>Ask a question</SubmitQButton>
-        </AskQuestionBar>
-      </Header>
-    )
+    if (!this.state.showModal) {
+      return (
+        <Header>
+          <TopSpanDiv>
+            <TopSpan>
+              <i className="far fa-comment-alt"></i>
+              <div>{this.props.questions.length}<br></br>Q&A</div>
+            </TopSpan>
+          </TopSpanDiv>
+          <LowerSpanDiv>
+            <Styling1>
+              <Styling2>
+              <QandA>
+                Questions & Answers
+                <SeeAll>See all {this.props.questions.length} questions</SeeAll>
+                <SubmitQButton onClick={() => this.showModal()}>Ask a question</SubmitQButton>
+              </QandA>
+            <AskQuestionBar>
+              {/* <TextField onChange={this.handleChange} value={this.state.question} name="question"></TextField> */}
+            </AskQuestionBar>
+              </Styling2>
+            </Styling1>
+          </LowerSpanDiv>
+        </Header>
+      )
+    } else {
+      return (
+        <Header>
+          <TopSpanDiv>
+            <TopSpan>
+              <i className="far fa-comment-alt"></i>
+              <div>{this.props.questions.length}<br></br>Q&A</div>
+            </TopSpan>
+          </TopSpanDiv>
+          <LowerSpanDiv>
+            <Styling1>
+              <Styling2>
+              <QandA>
+                Questions & Answers
+                <SeeAll>See all {this.props.questions.length} questions</SeeAll>
+                <SubmitQButton onClick={this.showModal}>Ask a question</SubmitQButton>
+              </QandA>
+            <AskQuestionBar>
+              {/* <TextField onChange={this.handleChange} value={this.state.question} name="question"></TextField> */}
+            </AskQuestionBar>
+            <Modal hideModal={this.hideModal}/>
+              </Styling2>
+            </Styling1>
+          </LowerSpanDiv>
+        </Header>
+      )
+    }
   }
 }
 
--- client/src/components/Questions.jsx
+++ client/src/components/Questions.jsx
@@ -3,32 +3,64 @@ import styled from 'styled-components';
 import Answers from './Answers.jsx';
 import AnswerQuestionBar from './AnswerQuestionBar.jsx';
 
-const Body = styled.div `
+const Body = styled.div`
   background-color: #f2f2f2;
   font-family: 'Poppins';
   ${Question}:nth-child(1) {
     margin-top: 16px;
   }
 `
-const QuestionModule = styled.div `
+const QuestionModule = styled.div`
   margin-bottom: 12px;
   background-color: rgb(255, 255, 255);
 `
-const Question = styled.div `
+const Question = styled.div`
   padding: 16px 24px 24px 12px;
   border: solid #e0e0e0;
   border-width: 0 0 1px;
 `
-const QuestionHeader = styled.div `
+const QuestionHeader = styled.div`
   margin-bottom: 16px;
   display: block;
 `
-const QuestionHeaderInfo = styled.div `
+const QuestionHeaderInfo = styled.div`
 display: block;
 `
-const AnswerBar = styled.form `
+const AnswerBar = styled.form`
 `
-const Buttons = styled.div `
+const Image = styled.img`
+height: 42px;
+width: 42px;
+border-radius: 100%;
+float: left;
+display: inline-block;
+margin: -3px 8px 0 -3px;
+`
+const QuestionBody = styled.div`
+  font-size: 16px;
+  line-height: 20px;
+`
+const Ellipsis = styled.i`
+  float: right;
+`
+const UserAndDate = styled.div`
+  padding-bottom: 2px;
+  font-size: 14px;
+  line-height: 18px;
+  color: #474747;
+`
+const LocationContVotes = styled.div`
+  font-size: 12px;
+  line-height: 16px;
+  color: #8c8c8c;
+`
+const Location = styled.span`
+`
+const NumContributions = styled.span`
+`
+const NumHelpfulVotes = styled.span`
+`
+const Buttons = styled.div`
   margin-bottom: 12px;
   background-color: rgb(255, 255, 255);
   padding: 16px 24px 24px 12px;
@@ -38,43 +70,46 @@ const Buttons = styled.div `
   border-left-width: 0px;
   border-bottom-width: 0px;
 `
-const Image = styled.img `
-height: 42px;
-width: 42px;
-border-radius: 100%;
-float: left;
+const ButtonsSpan = styled.div `
 display: inline-block;
-margin: -3px 8px 0 -3px;
 `
-const QuestionBody = styled.div `
-  font-size: 16px;
-  line-height: 20px;
+const PrevButton = styled.button`
+background-color: rgb(255, 255, 255);
+border-color: rgb(0, 0, 0);
+border-radius: 3px;
+border-width: 1px;
+color: rgb(0, 0, 0);
+display: inline-block;
+float: left;
+padding-bottom: 8px;
+padding-top: 8px;
+padding-right: 16px;
+padding-left: 16px;
+text-align: center;
+height: 36px;
+width: 77px;
 `
-const Ellipsis = styled.i `
-  float: right;
+const NextButton = styled.button`
+background-color: rgb(0, 0, 0);
+border-color: rgb(0, 0, 0);
+border-radius: 3px;
+border-width: 1px;
+color: rgb(255, 255, 255);
+display: inline-block;
+float: right;
+padding-bottom: 8px;
+padding-top: 8px;
+padding-right: 16px;
+padding-left: 16px;
+text-align: center;
+height: 36px;
+width: 77px;
 `
-const UserAndDate = styled.div `
-  padding-bottom: 2px;
-  font-size: 14px;
-  line-height: 18px;
-  color: #474747;
-`
-const LocationContVotes = styled.div `
-  font-size: 12px;
-  line-height: 16px;
-  color: #8c8c8c;
-`
-const Location = styled.span `
-`
-const NumContributions = styled.span `
-`
-const NumHelpfulVotes = styled.span `
-`
-const CircleIcon = styled.span `
+const CircleIcon = styled.span`
   font-size: 4px;
   vertical-align: middle;
 `
-const BoldSpan = styled.span `
+const BoldSpan = styled.span`
   font-weight: 500;
 `
 class Questions extends React.Component {
@@ -179,40 +214,42 @@ class Questions extends React.Component {
 
   render() {
     return (
-        <Body className='questionModule'>
-          {this.props.questions.map((question, questionIndex) => {
-            if (questionIndex >= this.state.questionMin && questionIndex < this.state.questionMax) return (
-              <QuestionModule key={questionIndex} className='question'>
-                <Question>
-                  <QuestionHeader>
-                    <Image src={`${question.profilePic}`} ></Image>
-                    <QuestionHeaderInfo>
-                      <Ellipsis className="fas fa-ellipsis-h"></Ellipsis>
-                      <UserAndDate>
-                        <div><BoldSpan>{question.username}</BoldSpan> asked a question {this.convertToMonthYear(question.date)}</div>
-                      </UserAndDate>
-                      <LocationContVotes>
-                        <span><i className="fas fa-map-marker-alt"></i></span><Location> {question.location} </Location>
-                        {question.numContributions > 0 ? <NumContributions><CircleIcon><i className="fas fa-circle"></i></CircleIcon><BoldSpan> {question.numContributions} </BoldSpan> contributions </NumContributions>: null}
-                        {question.numHelpfulVotes > 0 ? <NumHelpfulVotes><CircleIcon><i className="fas fa-circle"></i></CircleIcon><BoldSpan> {question.numHelpfulVotes} </BoldSpan> helpful votes</NumHelpfulVotes>: null}
-                      </LocationContVotes>
-                    </QuestionHeaderInfo>
-                  </QuestionHeader>
-                  <QuestionBody>
+      <Body className='questionModule'>
+        {this.props.questions.map((question, questionIndex) => {
+          if (questionIndex >= this.state.questionMin && questionIndex < this.state.questionMax) return (
+            <QuestionModule key={questionIndex} className='question'>
+              <Question>
+                <QuestionHeader>
+                  <Image src={`${question.profilePic}`} ></Image>
+                  <QuestionHeaderInfo>
+                    <Ellipsis className="fas fa-ellipsis-h"></Ellipsis>
+                    <UserAndDate>
+                      <div><BoldSpan>{question.username}</BoldSpan> asked a question {this.convertToMonthYear(question.date)}</div>
+                    </UserAndDate>
+                    <LocationContVotes>
+                      <span><i className="fas fa-map-marker-alt"></i></span><Location> {question.location} </Location>
+                      {question.numContributions > 0 ? <NumContributions><CircleIcon><i className="fas fa-circle"></i></CircleIcon><BoldSpan> {question.numContributions} </BoldSpan> contributions </NumContributions> : null}
+                      {question.numHelpfulVotes > 0 ? <NumHelpfulVotes><CircleIcon><i className="fas fa-circle"></i></CircleIcon><BoldSpan> {question.numHelpfulVotes} </BoldSpan> helpful votes</NumHelpfulVotes> : null}
+                    </LocationContVotes>
+                  </QuestionHeaderInfo>
+                </QuestionHeader>
+                <QuestionBody>
                   <div>{question.question}</div>
-                  </QuestionBody>
-                  <br></br>
-                </Question>
-                <Answers answers={question.answers} questionIndex={questionIndex} incrementVote={this.incrementVote} decrementVote={this.decrementVote}/>
-                <AnswerQuestionBar questionIndex={questionIndex} handleSubmit={this.handleSubmit}/>
-              </QuestionModule>
-            )
-          })}
-          <Buttons>
-            {this.state.currentPage > 1 ? <button onClick={this.prevPage}>Previous</button>: null}
-            {this.state.currentPage * 5 > this.props.questions.length ? null:<button onClick={this.nextPage}>Next</button>}
+                </QuestionBody>
+                <br></br>
+              </Question>
+              <Answers answers={question.answers} questionIndex={questionIndex} incrementVote={this.incrementVote} decrementVote={this.decrementVote} />
+              <AnswerQuestionBar questionIndex={questionIndex} handleSubmit={this.handleSubmit} />
+            </QuestionModule>
+          )
+        })}
+        <Buttons>
+          <ButtonsSpan>
+            {this.state.currentPage > 1 ? <PrevButton onClick={this.prevPage}>Previous</PrevButton> : null}
+            {this.state.currentPage * 5 > this.props.questions.length ? null : <NextButton onClick={this.nextPage}>Next</NextButton>}
+            </ButtonsSpan>
           </Buttons>
-        </Body>
+      </Body>
     )
   }
 }

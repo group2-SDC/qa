e2446239aa27f2b405dfa0ca19c442c48d56fd52
Matthew Crawford <matthew.crawford92@gmail.com>
2020-11-24 14:30:28 -0800

    Switched ReactDOM render path to service4

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('service4'));
+// switched from 'app' to 'service4'
+

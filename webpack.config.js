1659c9bec0678bc6c1e869df68a0d315bf488695
Matthew Crawford <matthew.crawford92@gmail.com>
2020-12-01 10:35:15 -0800

    Another webpack change

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,30 @@
 
 const path = require('path');
 
+// module.exports = {
+//   entry: '/client/src/index.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, '/client/dist'),
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx?/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader",
+//           options: {
+//             presets: ['@babel/preset-env', '@babel/preset-react']
+//           }
+//         }
+//       }
+//     ]
+//   }
+// };
+
 module.exports = {
-  entry: {
-    main: __dirname + '/client/src/index.jsx',
-    vendor: ['styled-components'],
-  }
+  entry: __dirname + '/client/src/index.jsx',
   mode: 'production',
   // switched mode to production
   module: {
@@ -25,15 +44,6 @@ module.exports = {
    output: {
     filename: 'bundle.js',
     path: __dirname + '/client/dist'
-  },
-  optimization: {
-    splitChunks: {
-      minChunks: Infinity,
-      name: true
-    },
-    runtimeChunk: {
-      name: 'vendor'
-    }
   }
 };
 
